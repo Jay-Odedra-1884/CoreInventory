5c3823fea1e6bdbad482407acb09131a60d1b1b2
Jay-Odedra-1884 <jayodedra9919@gmail.com>
2026-03-14 12:52:28 +0530

    ADD:  Warehouse, products migrations

---
+++ backend/database/migrations/2026_03_14_070355_create_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('sku');
+            $table->foreignId('category_id')->references('id')->on('product_category')->cascadeOnDelete();
+            $table->string('unit');
+            $table->string('price');
+            $table->string('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
